e4a026a9d5aa287a658c8318342c7fa091b9a759
Harmanen <hurmanen@gmail.com>
2022-12-13 22:32:26 +0200

    Fetch csv data to app state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css'
 
 const App = (props) => {
+
+  // All data
+  // const url = "https://covid.ourworldindata.org/data/owid-covid-data.csv"
+  
+  const url = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/hospitalizations/covid-hospitalizations.csv"
+
+  const [ rawData, setRawData ] = useState([])
+
+  fetch(url)
+    .then(response => response.text())
+    .then(text => setRawData(text))
+  
+  console.log(rawData)
 
   return <div className='App'>
     Add here
