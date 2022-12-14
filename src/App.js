4228b771122531255ced92670b13903f2ddfa820
Harmanen <hurmanen@gmail.com>
2022-12-14 18:13:56 +0200

    Add rudimentary data handling to App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css'
 
 const App = (props) => {
-
-  // All data
-  // const url = "https://covid.ourworldindata.org/data/owid-covid-data.csv"
+  const url = "https://covid.ourworldindata.org/data/owid-covid-data.csv"
   
-  const url = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/hospitalizations/covid-hospitalizations.csv"
-
-  const [ rawData, setRawData ] = useState([])
+  const [ rawData, setRawData ] = useState("")
 
   fetch(url)
     .then(response => response.text())
     .then(text => setRawData(text))
   
-  console.log(rawData)
+  useEffect(() => {
+    if (rawData.length > 0) {
+      // Make array of arrays per row from the csv
+      const rows = rawData.split("\n").map(item => item.split(","))
+
+      // Take the header row out
+      const headers = rows.shift()
+
+      // Add data handling later
+      // console.log(headers)
+      // console.log(rows)
+  }}, [rawData])
 
   return <div className='App'>
     Add here
