938107f4ba6fde48c9bd19d59020186c4b306aab
Harmanen <hurmanen@gmail.com>
2022-12-18 18:29:31 +0200

    Add a missing return type

--- src/functions/helpers.ts
+++ src/functions/helpers.ts
@@ -1,4 +1,4 @@
 // A helper function to find correct array indices
-export const findVariableIndex = (headers: [], keyword: string) => {
+export const findVariableIndex = (headers: [], keyword: string): number => {
   return headers.findIndex(item => item === keyword)
 }
