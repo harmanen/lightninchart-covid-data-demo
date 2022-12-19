8d4865c8780443d2b94447d767fb3fb78c4c1ca6
Harmanen <hurmanen@gmail.com>
2022-12-19 19:21:55 +0200

    Implement Dashboard layout
    Separate legend from the main plot.

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -15,71 +15,62 @@ const Chart = (props) => {
   const { data, id, yMax } = props
 
   useEffect(() => {
-    // Create chart, series and any other static components.
-    // NOTE: console log is used to make sure that chart is only created once, even if data is changed!
-    console.log('create chart')
-    const chart = lightningChart().ChartXY({ container: id })
+    // Create Dashboard and stand-alone LegendBox.
+    const db = lightningChart().Dashboard({
+      numberOfRows: 3,
+      numberOfColumns: 1,
+      container: id
+    })
     
-    chart.setTitle(CHART_TITLE)
+    // Create a legendBox docked to the Dashboard.
+    const legend = db.createLegendBoxPanel({
+        columnIndex: 0,
+        rowIndex: 2,
+        columnSpan: 1,
+        rowSpan: 1
+    })
 
-    // Use DateTime X-axis with origin
-    chart
-    .getDefaultAxisX()
-    .setTickStrategy(
-      AxisTickStrategies.DateTime,
-      (tickStrategy) => tickStrategy.setDateOrigin(DATE_ORIGIN)
-    )
+    // Chart
+    {
+      const chart = db.createChartXY({
+        columnIndex: 0,
+        rowIndex: 0,
+        columnSpan: 1,
+        rowSpan: 2
+      })
 
-    // Use default y axis
-    chart
-      .getDefaultAxisY()
-      .setTitle("Number of patients (normalized)")
-      .setInterval(
-        0,
-        // Increse max value by 10 % to get some "air"
-        yMax.current * 1.1
-      ) 
+      chart.setTitle(CHART_TITLE)
 
-    // Dynamically add lines
-    const seriesList = Object
+      // Use DateTime X-axis with origin
+      chart
+        .getDefaultAxisX()
+        .setTickStrategy(
+          AxisTickStrategies.DateTime,
+          (tickStrategy) => tickStrategy.setDateOrigin(DATE_ORIGIN)
+      )
+  
+      // Use default y axis
+      chart
+        .getDefaultAxisY()
+        .setTitle("Number of patients (normalized)")
+        .setInterval(
+          0,
+          // Increse max value by 10 % to get some "air"
+          yMax.current * 1.1
+        ) 
+
+      // Dynamically add lines
+      Object
       .entries(data)
       .map(([country, values]) => chart
         .addLineSeries()
         .setName(country)
         .add(values))
-
-    // Add multi-line legend
-    // https://stackoverflow.com/questions/70271221/how-to-create-horizontal-multiline-legend
-    const legendLayout = chart
-      .addUIElement(UILayoutBuilders.Column)
-      .setPosition({ x: 0, y: 100 })
-      .setOrigin(UIOrigins.LeftTop)
-      .setMargin(10)
-      .setDraggingMode(UIDraggingModes.disabled)
-  
-    const numberOfRows = 16
-    
-    const legendList = new Array(numberOfRows).fill(0).map(_ => legendLayout
-      .addElement(LegendBoxBuilders.HorizontalLegendBox)
-      .setTitle('')
-      .setMargin(0)
-      .setPadding(0)
-    )
-
-    let legendIndex = 0
-
-    for (let index = 0; index < seriesList.length; index++) {
-      if (index > 0 && index % numberOfRows === 0) legendIndex++
       
-      // Mainly for debugging
-      try {
-        legendList[legendIndex].add(seriesList[index])
-      } catch (error) {
-        console.log("Something went wrong with legend creation, skippind:")
-        console.log(index, seriesList[index])
-        console.log(error)  
-      }
+      // Add to LegendBox
+      legend.add(chart)
     }
+
   }, [id, data, yMax])
 
   return <div id={id} className='chart'></div>
